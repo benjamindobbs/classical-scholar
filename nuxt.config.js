83420e74c8ce0dd5782eb7c99f2a45dcf5d8d966
Dobbs <benjamindobbs@gmail.com>
2021-11-28 15:13:01 -0500

    adding repo name for ghpages

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
     router: {
-      base: '/<repository-name>/'
+      base: '/classical-scholar/'
     },
   head: {
     titleTemplate: '%s - classical-scholar-nuxt',
