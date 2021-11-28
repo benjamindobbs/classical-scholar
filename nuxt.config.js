0c5d416febb6517a1d7a8a9a51c5a3e4b517f8c9
Dobbs <benjamindobbs@gmail.com>
2021-11-28 15:09:15 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+    router: {
+      base: '/<repository-name>/'
+    },
   head: {
     titleTemplate: '%s - classical-scholar-nuxt',
     title: 'classical-scholar-nuxt',
