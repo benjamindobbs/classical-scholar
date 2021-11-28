37b4636c93100a8aa554332291a8f35ee08b3e04
Dobbs <benjamindobbs@gmail.com>
2021-11-28 16:36:46 -0500

    removed router base
    Didn't need it

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
     router: {
-      base: '/classical-scholar/'
+      base: '/'
     },
   head: {
     titleTemplate: '%s - classical-scholar-nuxt',
