d8595ed0025543e0c7acd49d348cfcc0793e47b6
benjamindobbs <benjamindobbs@gmail.com>
2021-12-03 16:12:04 -0500

    Changed the header for contributers

--- pages/contributers.vue
+++ pages/contributers.vue
@@ -1,7 +1,8 @@
 <template>
 <v-app>
     <v-main>
-    <h1 style="color:black;"> Placeholder for Contributors </h1>
+    <h1 style="color:black;"> Classical Scholar is a Student Run Site </h1>
+    <h2 class="subheading;"> These are the students who have contributed </h2> 
     </v-main>
 </v-app>
 </template>
