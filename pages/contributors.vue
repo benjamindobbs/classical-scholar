4d4c6e4f945b59ec091c6759a2887d7e8c188d48
benjamindobbs <benjamindobbs@gmail.com>
2022-03-31 07:17:50 -0400

    added faculty advisorys to contributors page

--- pages/contributors.vue
+++ pages/contributors.vue
@@ -4,7 +4,7 @@
     <v-card>
         <v-card-title> Our Contributors </v-card-title>
         <v-img src="../images/January-22/contributors.png"> </v-img>
-        <v-card-text> From Left to Right: Adrianna Jimenez, Reporter; Uzoma Chukwurah, Reporter; Ashley Nelson, Editor; Josiah Perez-Henry, Photographer </v-card-text>
+        <v-card-text> From Left to Right: Adrianna Jimenez, Reporter; Uzoma Chukwurah, Reporter; Ashley Nelson, Editor; Josiah Perez-Henry, Photographer. Not Pictured: John Hill, Faculty Advisor; Dobbs, Programmer/Developer </v-card-text>
     </v-card> 
     </v-main>
 </v-app>
